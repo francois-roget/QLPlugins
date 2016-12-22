7a4c415e419ab3c60b18da5c7502b96353736fd2
François ROGET <fro@pulsar.be>
2016-12-22 15:06:34 +0100

    initial commit

---
+++ installQLPlugins.sh
@@ -0,0 +1,18 @@
+brew install caskroom/cask/brew-cask
+
+brew cask install qlcolorcode
+brew cask install qlstephen
+brew cask install qlmarkdown
+brew cask install quicklook-json
+brew cask install qlprettypatch
+brew cask install quicklook-csv
+
+# brew cask install dropbox
+# brew cask install textwrangler
+# brew cask install google-chrome
+# brew cask install mysqlworkbench
+# brew cask install iterm2 spectacle slack spotify the-unarchiver pgadmin3 firefox
+# brew cask install viscosity
+# brew cask install parallels-desktop
+# brew cask install forklift
+# brew cask install displaylink
